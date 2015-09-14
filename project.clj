6318c0f7424c1ed9c19062b90f9950d4649163fc
Laurens Van Houtven <_@lvh.io>
2015-09-14 13:31:49 -0700

    Skeleton

---
+++ project.clj
@@ -0,0 +1,37 @@
+(defproject bubbles "0.1.0-SNAPSHOT"
+  :description "In-browser Clojurescript SOAP client library"
+  :url "https://github.com/racksec/bubbles"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.3.9"]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+
+              :figwheel { :on-jsload "bubbles.core/on-js-reload" }
+
+              :compiler {:main bubbles.core
+                         :asset-path "js/compiled/out"
+                         :output-to "resources/public/js/compiled/bubbles.js"
+                         :output-dir "resources/public/js/compiled/out"
+                         :source-map-timestamp true }}
+             {:id "min"
+              :source-paths ["src"]
+              :compiler {:output-to "resources/public/js/compiled/bubbles.js"
+                         :main bubbles.core
+                         :optimizations :advanced
+                         :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"] ;; watch and update CSS
+             :nrepl-port 7002})
