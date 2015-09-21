78ab372f0779e5a43d01ae8dad3679ba244593fc
Laurens Van Houtven <_@lvh.io>
2015-09-21 15:51:24 -0700

    Whitespace consistency

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                          :asset-path "js/compiled/out"
                          :output-to "resources/public/js/compiled/bubbles.js"
                          :output-dir "resources/public/js/compiled/out"
-                         :source-map-timestamp true }}
+                         :source-map-timestamp true}}
 
              {:id "min"
               :source-paths ["src"]
