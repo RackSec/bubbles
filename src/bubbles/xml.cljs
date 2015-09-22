5617423680c4bca1253430517d28c6b5e82a79ee
Laurens Van Houtven <_@lvh.io>
2015-09-22 15:26:04 -0700

    Add stubs

---
+++ src/bubbles/xml.cljs
@@ -0,0 +1,16 @@
+(ns bubbles.xml
+  "Fanciness related to parsing and serializing XML."
+  (:require [hickory.core :as h]))
+
+(defn parse-xml
+  "Parse an XML string into DOM objects.
+
+  TODO: consider submitting this upstream to Hickory; maybe give
+  parse-dom-with-domparser an optional doctype arg."
+  [s]
+  (.parseFromString (js/DOMParser.) s "text/xml"))
+
+(defn ->xml
+  "Turns a Clojure data structure into some XML."
+  [data]
+  )
