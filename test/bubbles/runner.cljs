9cc527b4676bb7b18822218f99487d7ba81c172b
Laurens Van Houtven <_@lvh.io>
2015-09-22 16:43:08 -0700

    Add xml-test to runner

--- test/bubbles/runner.cljs
+++ test/bubbles/runner.cljs
@@ -1,5 +1,7 @@
 (ns bubbles.runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            [bubbles.core-test]))
+            [bubbles.core-test]
+            [bubbles.xml-test]))
 
-(doo-tests 'bubbles.core-test)
+(doo-tests 'bubbles.core-test
+           'bubbles.xml-test)
